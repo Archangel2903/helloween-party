e7e4d8b33f186b212742ece2ddc3d63196bf0629
Dmitriy <archangel2903@gmail.com>
2024-10-10 07:21:32 +0300

    fin

--- src/js/index.js
+++ src/js/index.js
@@ -14,36 +14,18 @@ $(window).on('load', function () {
 });
 
 $(function () {
-    // Указываем дату и время окончания
-    const eventDate = new Date('2024-10-31T18:00:00').getTime();
+    $(window).on('resize', function (e) {
+        console.log(e.target.innerWidth);
 
-    // Функция обратного отсчёта
-    function countdownTimer() {
-        const now = new Date().getTime();
-        const timeRemaining = eventDate - now;
-
-        // Вычисляем дни, часы и минуты
-        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-
-        // Отображаем результат на странице
-        document.getElementById('days').textContent = days < 10 ? '0' + days : days;
-        document.getElementById('hours').textContent = hours < 10 ? '0' + hours : hours;
-        document.getElementById('minutes').textContent = minutes < 10 ? '0' + minutes : minutes;
-
-        // Если время истекло
-        if (timeRemaining < 0) {
-            clearInterval(countdownInterval);
-            document.getElementById('days').textContent = '00';
-            document.getElementById('hours').textContent = '00';
-            document.getElementById('minutes').textContent = '00';
+        if (e.target.innerWidth >= 1024) {
+            $('body').removeClass('overflow-hidden');
+            $('.header-content__burger').removeClass('active');
+            $('.header-content-inside').removeClass('active');
         }
-    }
-
-    // Запускаем обновление каждую минуту
-    const countdownInterval = setInterval(countdownTimer, 1000);
-
-    // Запускаем первый раз сразу
-    countdownTimer();
+    });
+    $('.header-content__burger').on('click', function () {
+        $('body').toggleClass('overflow-hidden');
+        $(this).toggleClass('active');
+        $(this).next().toggleClass('active');
+    });
 });
